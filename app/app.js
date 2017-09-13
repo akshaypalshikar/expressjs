3b17f7607320d32b4574ed863b2d45ae8ce48d0b
Akshay Palshikar <aksmakesit@gmail.com>
2017-09-12 21:20:23 -0700

    NodeJs Http server

--- app/app.js
+++ app/app.js
@@ -4,6 +4,7 @@ var serverHandler = function(request,response){
     response.writeHead(200,{"Content-Type":"text/plain"});
     response.write("Meetups");
     response.end();
+    
 }
 
 var server = http.createServer(serverHandler);
