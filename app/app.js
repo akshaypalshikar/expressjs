fa467b341033b90bd65b409a4fe16d70e666b331
Akshay Palshikar <aksmakesit@gmail.com>
2017-09-12 21:18:25 -0700

    NodeJs Http server

---
+++ app/app.js
@@ -0,0 +1,14 @@
+var http=require("http");
+
+var serverHandler = function(request,response){
+    response.writeHead(200,{"Content-Type":"text/plain"});
+    response.write("Meetups");
+    response.end();
+}
+
+var server = http.createServer(serverHandler);
+
+
+
+server.listen(3000);
+console.log("Server is running!\nGo to http://localhost:3000 on your browser.");
