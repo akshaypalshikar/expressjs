be4596a2dd7d70b54ded99fe85b1891adeb1876e
Akshay Palshikar <aksmakesit@gmail.com>
2017-09-21 22:13:36 -0700

    basic routing with expressjs added.

--- app/app.js
+++ app/app.js
@@ -1,15 +1,50 @@
-var http=require("http");
+var express=require("express");
+var app=express();
+var SERVER_HTTP_PORT = 3000;
+var dataFile = require('./data/data.json');
+var PORT_STR = 'port';
 
-var serverHandler = function(request,response){
-    response.writeHead(200,{"Content-Type":"text/plain"});
-    response.write("Meetups");
-    response.end();
-    
+var homePageHandler = function(request,response){
+    response.send(`<H1>Welcome</H1>
+    <p>Meetups put together artists from all walks of life.</p>`);
 }
 
-var server = http.createServer(serverHandler);
+var speakersPageHandler = function(request,response){
+    var info = "";
+    dataFile.speakers.forEach(function(element) {
+        info += `
+                <li>
+                    <h2>${element.name}</h2>
+                    <p>${element.summary}</p>
+                </li>
+        `;
+    });
+    response.send(`<H1>Meetups</H1>
+        ${info} 
+    `);
+}
+
+var speakerByIdPageHandler = function(request,response){
+    var info = "";
+    var speaker = dataFile.speakers[request.params.speakerId];
+    response.send(`<h1>${speaker.title}</h1>
+                   <h2> ${speaker.name}</h2>
+                   <p>${speaker.summary}</p>
+    `);
+}
+
+var serverHandler = function(){
+    console.log("Server is running!\nGo to http://localhost:"+app.get(PORT_STR)+" on your browser.");
+}
+app.set(PORT_STR, process.env.PORT || SERVER_HTTP_PORT);
+
+/**
+ * All the routes are here.
+ */
+app.get("/",homePageHandler);
+app.get("/speakers",speakersPageHandler);
+app.get("/speaker/:speakerId",speakerByIdPageHandler);
 
 
 
-server.listen(3000);
-console.log("Server is running!\nGo to http://localhost:3000 on your browser.");
+var server = app.listen(app.get(PORT_STR),serverHandler);
